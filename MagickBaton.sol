ae26d1168fa115145d1f4e85c5feb116d5218fc2
Stamford Hwang <stamfordh@hotmail.com>
2020-05-11 15:21:40 -0400

    Update MagickBaton.sol

---
+++ MagickBaton.sol
@@ -0,0 +1,35 @@
+pragma solidity 0.5.14;
+
+interface IToken { // brief ERC-20 interface
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+}
+
+contract LexDAOMagickBatonSpell01 {
+    address public accessToken = 0x027fa7C653fBfD1Fe49Bcf930a0E40fc429bCC72; // $LXDB
+    address public leethToken = 0x4D9D9a22458dD84dB8B0D074470f5d9536116eC5; // $LXK
+    address public executioner;
+
+    IToken private token = IToken(accessToken);
+    IToken private leeth = IToken(leethToken);
+
+    function castSpell() public { // $LXDB holder can cast spell to withdraw balance of deposited $LXK
+        require(token.balanceOf(msg.sender) >= 1, "token balance insufficient");
+        leeth.transfer(msg.sender, leeth.balanceOf(address(this)));
+    }
+
+    function handoffLXDB(address payable receiver) public {
+        require(token.balanceOf(msg.sender) >= 1, "token balance insufficient");
+        token.transfer(receiver, token.balanceOf(msg.sender));
+    }
+
+    function assignExecutioner(address _executioner) public {
+        require(token.balanceOf(msg.sender) >= 1, "token balance insufficient");
+        executioner = _executioner;
+    }
+
+    function execute(address payable receiver) public {
+        require(msg.sender == executioner);
+        leeth.transfer(receiver, leeth.balanceOf(address(this)));
+    }
+}
